feb9e08926969af90dbb54287aa793f656b4d5fa
miffy24 <1248378683@qq.com>
2018-02-03 18:49:25 +0800

    优化

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
-$('.images > img:nth-child(1)').addClass('current')
-$('.images > img:nth-child(2)').addClass('enter')
-$('.images > img:nth-child(3)').addClass('enter')
-$('.images > img:nth-child(4)').addClass('enter')
-$('.images > img:nth-child(5)').addClass('enter')
-
 let n = 1
+getInit()
 setInterval(()=>{
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+    makeLeave(getImage(n))
     .one('transitionend', (e)=>{
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n+1))
   n += 1
 },3000)
 
-
+function getImage(n){
+	return $(`.images >img:nth-child(${x(n)})`)
+}
 function x(n){
 	if(n > 5){
 		n = n % 5
@@ -23,4 +20,19 @@ function x(n){
 		}
 	}
 	return n
+}
+function getInit(){
+	n = 1
+	$(`.images >img:nth-child(${n})`).addClass('current')
+	.siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+	return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+	return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+	return $node.removeClass('leave current ').addClass('enter')
 }
